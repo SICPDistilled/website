a1dd283e45dbbe529c85bfa5bfe52dfbc765e415
thattommyhall <thattommyhall@gmail.com>
2014-12-01 01:35:57 +0000

    init

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source "https://rubygems.org"
+
+gem 'sinatra-contrib'
+gem 'sinatra'
+gem 'omniauth'
+gem 'omniauth-github'
+gem "octokit"
+gem 'thin'
+gem 'haml'
---
+++ server.rb
@@ -0,0 +1,65 @@
+require 'sinatra'
+require 'omniauth'
+require 'omniauth-github'
+require 'octokit'
+require 'haml'
+require "sinatra/reloader" if development?
+
+use Rack::Session::Cookie
+
+set :session_secret, ENV['SESSION_SECRET'] || 'sssshhhh'
+
+use OmniAuth::Builder do
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+end
+
+def github
+  @github ||= Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
+end
+
+def authenticated?
+  return true if settings.development?
+  session[:authenticated] && in_org?(session[:user_id])
+end
+
+def in_org?(id)
+  github.organization_member?('SICPDistilled', id)
+end
+
+def require_logon!
+  redirect '/sign-in' unless authenticated?
+end
+
+get '/sign-in' do
+  haml :sign_in
+end
+
+get '/' do
+  require_logon!
+  haml :index
+end
+
+get '/slides/:id/' do
+  require_logon!
+  @id = params[:id]
+  erb :presentation
+end
+
+get '/auth/:provider/callback' do
+  session[:authenticated] = true
+  session[:user_id] = request.env['omniauth.auth']['info']['name']
+  redirect '/'
+end
+
+get '/auth/failure' do
+  haml :auth_failed
+end
+
+get '/auth/:provider/deauthorized' do
+  haml :deauthorized
+end
+
+get '/logout' do
+  session[:authenticated] = false
+  redirect '/'
+end
