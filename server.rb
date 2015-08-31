fc004573002d7e442feaf3bb04ec90bdc03c96a1
thattommyhall <thattommyhall@gmail.com>
2015-08-31 19:00:05 +0100

    what even is authentication?

--- server.rb
+++ server.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
-require 'omniauth'
-require 'omniauth-github'
-require 'octokit'
+#require 'omniauth'
+#require 'omniauth-github'
+#require 'octokit'
 require 'haml'
 require 'redcarpet'
 require 'sinatra/reloader' if development?
@@ -25,9 +25,9 @@ end
 
 set :session_secret, ENV['SESSION_SECRET'] || 'sssshhhh'
 
-use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
-end
+# use OmniAuth::Builder do
+#   provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+# end
 
 def environment
   ENV['RACK_ENV']
@@ -37,26 +37,26 @@ before do
   cache_control :public, :must_revalidate, :max_age => 600
 end
 
-def github
-  @github ||= Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
-end
+# def github
+#   @github ||= Octokit::Client.new(access_token: ENV['GITHUB_TOKEN'])
+# end
 
-def authenticated?
-  return true if settings.development?
-  session[:authenticated] && in_org?(session[:user_id])
-end
+# def authenticated?
+#   return true if settings.development?
+#   session[:authenticated] && in_org?(session[:user_id])
+# end
 
-def in_org?(id)
-  github.organization_member?('SICPDistilled', id)
-end
+# def in_org?(id)
+#   github.organization_member?('SICPDistilled', id)
+# end
 
-def require_logon!
-  redirect '/sign-in' unless authenticated?
-end
+# def require_logon!
+#   redirect '/sign-in' unless authenticated?
+# end
 
-get '/sign-in' do
-  haml :sign_in
-end
+# get '/sign-in' do
+#   haml :sign_in
+# end
 
 get '/' do
   redirect '/section/1-distilled'
@@ -80,21 +80,21 @@ get '/slides/:id/' do
   erb :presentation
 end
 
-get '/auth/:provider/callback' do
-  session[:authenticated] = true
-  session[:user_id] = request.env['omniauth.auth']['info']['nickname']
-  redirect '/'
-end
+# get '/auth/:provider/callback' do
+#   session[:authenticated] = true
+#   session[:user_id] = request.env['omniauth.auth']['info']['nickname']
+#   redirect '/'
+# end
 
-get '/auth/failure' do
-  haml :auth_failed
-end
+# get '/auth/failure' do
+#   haml :auth_failed
+# end
 
-get '/auth/:provider/deauthorized' do
-  haml :deauthorized
-end
+# get '/auth/:provider/deauthorized' do
+#   haml :deauthorized
+# end
 
-get '/logout' do
-  session[:authenticated] = false
-  redirect '/'
-end
+# get '/logout' do
+#   session[:authenticated] = false
+#   redirect '/'
+# end
