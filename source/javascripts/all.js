e1df6b7ad2bd2c5b9eacaa696a61f8355ffa9ed1
David Annez <david.annez@gmail.com>
2015-09-09 23:30:33 +0100

    Cleaner Sass modules, menu fixes for mobile, removal of jquery

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,20 @@
 //= require_tree .
+
+var mobileNav = document.querySelector(".js-mobile-nav");
+
+mobileNav.addEventListener("click", clickToggle, false);
+
+function clickToggle(event){
+  var toggleLink = this;
+  var target = document.querySelector("." + toggleLink.getAttribute("data-target"));
+  var targetActiveClass = getActiveClass(target);
+  var activeClass = getActiveClass(toggleLink);
+  toggleLink.classList.toggle(activeClass);
+  target.classList.toggle(targetActiveClass);
+}
+
+function getActiveClass(selector){
+  return selector.classList[0] + "--active";
+}
+
+hljs.initHighlightingOnLoad();
